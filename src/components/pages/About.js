23e16adbbb624b7f9f185cb2cb216039f35befcf
Sidhant Puntambekar <sidhantnp@yahoo.com>
2021-11-20 17:10:19 -0700

    Added accent colors

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -27,18 +27,25 @@ const About = () => {
   return (
     <div style={style}>
       <h2>About Me</h2>
-      <div stlye={{ float: "right" }}>
-        <h4 stlye={{ marginBottom: "25px" }}>
+      <div>
+        <h4 style={{ margin: "20px" }}>
           Welcome to my portfolio website! My name is Sidhant Puntambekar and I
           am currently a sophomore undergraduate at the University of Colorado,
-          Boulder. I am currently majoring in Computer Science and plan to minor
+          Boulder.  
+        </h4>
+        <h4 style={{ margin: "20px" }}>
+            I am currently majoring in Computer Science and am minoring
           in Molecular, Cellular, and Developmental Biology and Computational
-          Biology. While my main passions are in the areas of Computer Science
+          Biology.
+        </h4>
+        <h4 style={{ margin: "20px" }}>
+            While my main passions are in the areas of Computer Science
           and Molecular Biology, I am infatuated with learning about all subject
           areas. Some of my other favorite activities are chess, reading up on
           military history, playing the guitar, playing the drums, and doing Tae
           Kwon Do. I also enjoy listening to music and watching movies.
         </h4>
+          
         <a href={Resume} without rel="noopener noreferrer" target="_blank">
           <button
             trailingIcon="picture_as_pdf"
