22e5b89a1ee5c8db842d7b8d9ed5edfdb1f48f3f
Sidhant Puntambekar <sidhantnp@yahoo.com>
2023-04-16 11:50:30 -0600

    Updated travel section with full list of places visited

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -16,13 +16,19 @@ const Travel = () => {
         display: "inline-block",
         overflow: "hidden"
     }
+    
+    const iframeStyle = {
+        position: "relative",
+        top: "-50px",
+        border: "none"
+    }
 
     return (
         <div style={style}>
             <h2>Travel</h2>
             <h3>Traveling is one of my favorite ways to spend time outside of work and academics. Every time I visit a new place, I'll drop a pin on the map below. My goal is to eventually visit every state in the US as well as every national park and presidential library. Internationally, I plan to travel to as many countries in the world as possible.</h3>
             <div style={mapStyle}>
-                <iframe src="https://www.google.com/maps/d/embed?mid=1rh0t5ra8Fu3X0RfWutBt41WZYL9zKNs&ehbc=2E312F" width="675" height="550"></iframe>
+                <iframe style={iframeStyle} src="https://www.google.com/maps/d/embed?mid=1rh0t5ra8Fu3X0RfWutBt41WZYL9zKNs&ehbc=2E312F" width="675" height="550"></iframe>
             </div>
         
         </div>
