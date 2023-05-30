951b7af40082bf84dd90ed07b6ec8c08d068767f
Sidhant Puntambekar <sidhantnp@yahoo.com>
2023-05-29 22:56:50 -0600

    Add new website content

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,6 +5,13 @@ import roxborough from '../img/roxborough.JPG'
 import lajolla from '../img/lajolla.JPG'
 import yosemite from '../img/yosemite.jpg'
 import deathvalley from '../img/deathvalley.jpg'
+import cabo from '../img/cabo.JPG'
+import maui from '../img/maui.jpg'
+import marienplatz from '../img/marienplatz.jpg'
+import neuschwanstein from '../img/neuschwanstein.jpg'
+import hallstatt from '../img/hallstatt.jpg'
+import salzburg from '../img/salzburg.jpg'
+import olympiastadion from '../img/olympiastadion.jpg'
 
 const Carousel = () => {
   const style = {
@@ -43,6 +50,41 @@ const Carousel = () => {
             <input type="radio" id="img-5" name="gallery" class="gallery__selector"/>
             <img class="gallery__img" src={deathvalley} alt="Death Valley"/>
             <label for="img-5" class="gallery__thumb"><img src={deathvalley} alt="Death Valley"/></label>
+          </div>
+          <div class="radio-buttons gallery__item">
+            <input type="radio" id="img-6" name="gallery" class="gallery__selector"/>
+            <img class="gallery__img" src={cabo} alt="Cabo San Lucas"/>
+            <label for="img-6" class="gallery__thumb"><img src={cabo} alt="Cabo San Lucas"/></label>
+          </div>
+          <div class="radio-buttons gallery__item">
+            <input type="radio" id="img-7" name="gallery" class="gallery__selector"/>
+            <img class="gallery__img" src={maui} alt="Makena Beach, Maui"/>
+            <label for="img-7" class="gallery__thumb"><img src={maui} alt="Makena Beach, Maui"/></label>
+          </div>
+          <div class="radio-buttons gallery__item">
+            <input type="radio" id="img-8" name="gallery" class="gallery__selector"/>
+            <img class="gallery__img" src={marienplatz} alt="Marienplatz, Munich"/>
+            <label for="img-8" class="gallery__thumb"><img src={marienplatz} alt="Marienplatz, Munich"/></label>
+          </div>
+          <div class="radio-buttons gallery__item">
+            <input type="radio" id="img-9" name="gallery" class="gallery__selector"/>
+            <img class="gallery__img" src={neuschwanstein} alt="Neuschwanstein Schloss"/>
+            <label for="img-9" class="gallery__thumb"><img src={neuschwanstein} alt="Neuschwanstein Schloss"/></label>
+          </div>
+          <div class="radio-buttons gallery__item">
+            <input type="radio" id="img-10" name="gallery" class="gallery__selector"/>
+            <img class="gallery__img" src={hallstatt} alt="Hallstatt"/>
+            <label for="img-10" class="gallery__thumb"><img src={hallstatt} alt="Hallstat"/></label>
+          </div>
+          <div class="radio-buttons gallery__item">
+            <input type="radio" id="img-11" name="gallery" class="gallery__selector"/>
+            <img class="gallery__img" src={olympiastadion} alt="Olympic Park, Munich"/>
+            <label for="img-11" class="gallery__thumb"><img src={olympiastadion} alt="Olympic Park, Munich"/></label>
+          </div>
+          <div class="radio-buttons gallery__item">
+            <input type="radio" id="img-12" name="gallery" class="gallery__selector"/>
+            <img class="gallery__img" src={salzburg} alt="Salzburg"/>
+            <label for="img-12" class="gallery__thumb"><img src={salzburg} alt="Salzburg"/></label>
           </div>
         </section>
       </div>
